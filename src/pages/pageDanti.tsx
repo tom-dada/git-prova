42aa37916f25378cd4bf613eff7d9a2bdacf7de2
Tommaso Dario Danti <tommasodanti@yahoo.it>
2023-11-06 14:43:37 +0100

    feat(pages): aggiunto paragrafo con soluzione in pageDanti

--- src/pages/pageDanti.tsx
+++ src/pages/pageDanti.tsx
@@ -5,6 +5,11 @@ export const PageDanti = () => {
     <div>
       <Logo />
       <h1>Tommaso Danti</h1>
+      <p>
+        Ho fatto checkout sulla branch personale
+        Poi ho fatto una merge sul main
+        Poi ho fatto checkout sul main e ho fatto un reset alla commit precedente
+      </p>
     </div>
   );
 };
